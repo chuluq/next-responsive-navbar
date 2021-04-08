2979e4567b7c56b8d10ab4f8bfa071ed57de1ad8
chuluq <49191833+chuluq@users.noreply.github.com>
2021-04-08 14:09:32 +0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,59 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
-export default function Home() {
+const Navbar = () => {
+  const [showLink, setShowLink] = useState(false);
+
+  const toggleLink = () => {
+    setShowLink(!showLink);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <header className='flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-white py-6 px-6 relative shadow-md'>
+      <a href='#' className='block'>
+        <h1 className='font-bold text-lg'>Logo</h1>
+      </a>
+      <button
+        onClick={toggleLink}
+        className='inline-block md:hidden w-8 h-8 bg-gray-200 text-gray-600 p-1'
+      >
+        <svg
+          fill='currentColor'
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <path
+            fillRule='evenodd'
+            d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+            clipRule='evenodd'
+          ></path>
+        </svg>
+      </button>
+      <nav
+        className={`${
+          showLink ? '' : 'hidden'
+        }  md:inline-flex absolute md:relative top-16 left-0 md:top-0 z-20 flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0`}
+      >
+        <a
+          href='#'
+          className='block py-1 px-2 text-indigo-600 hover:bg-gray-100'
+        >
+          Home
         </a>
-      </footer>
-    </div>
-  )
-}
+        <a href='#' className='block py-1 px-2 text-gray-600 hover:bg-gray-100'>
+          About us
+        </a>
+        <a href='#' className='block py-1 px-2 text-gray-600 hover:bg-gray-100'>
+          Services
+        </a>
+        <a href='#' className='block py-1 px-2 text-gray-600 hover:bg-gray-100'>
+          Blog
+        </a>
+        <a href='#' className='block py-1 px-2 text-gray-600 hover:bg-gray-100'>
+          Contact
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
